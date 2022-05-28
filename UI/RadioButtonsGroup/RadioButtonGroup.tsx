c5d0991865ca87134388266e37ee69714a47bd3e
Artem <artemmorozov1308@gmail.com>
2022-05-28 23:47:25 +0300

    added svg background

--- UI/RadioButtonsGroup/RadioButtonGroup.tsx
+++ UI/RadioButtonsGroup/RadioButtonGroup.tsx
@@ -40,9 +40,10 @@ const RadioButtonGroup: FC<RadioButtonGroupProps> = (props) => {
             {elements.map(({value, label}, index) => {
                 return(
                     <FormControlLabel
+                        key={index}
                         className={s.radio_button}
                         value={value}
-                        control={<Radio />}
+                        control={<Radio checkedIcon={<SvgFill/>} />}
                         label={label}
                     />
                 )
@@ -50,6 +51,17 @@ const RadioButtonGroup: FC<RadioButtonGroupProps> = (props) => {
             </RadioGroup>
         </div>
         
+    )
+}
+
+const SvgFill = () => {
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+            <g data-name="Ellipse 538" fill="#8100ef">
+                <circle cx="10" cy="10" r="10" style={{stroke:'none'}}/>
+                <circle cx="10" cy="10" r="9.25" style={{fill:'none'}}/>
+            </g>
+        </svg>
     )
 }
 
