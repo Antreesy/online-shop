98417840e221c7d56755c9986fb3ca8c531b3c8a
Максим Сухарев <tris-z@mail.ru>
2022-05-30 23:24:55 +0700

    fix link and styles

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,22 +1,26 @@
 import { NextPage } from "next"
-import React from "react"
+
+import Link from "next/link"
 import { Button } from "UI"
+
 import s from "../styles/pages/page-not-found.module.scss"
 
 const PageNotFound: NextPage = () => {
   return (
-      <>
-    <div className={s.background}>
-      
-    </div>
-    <div className={s.content}>
+    <>
+      <div className={s.background}></div>
+      <div className={s.content}>
         <h1>Page Not Found</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius
           porta pharetra. Curabitur dolor sit amet venenatis purus ante.
         </p>
         <br />
-        <Button className={s.button_purple}>Return to homepage</Button>
+        <Link href={"/"}>
+          <a>
+            <Button className={s.button_purple}>Return to homepage</Button>
+          </a>
+        </Link>
       </div>
     </>
   )
