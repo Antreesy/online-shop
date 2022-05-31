979908e0a10cc274f3e817db9d255937298f2cd9
Максим Сухарев <tris-z@mail.ru>
2022-06-01 00:28:05 +0700

    fix props

--- Сomponents/Notification/Notification.tsx
+++ Сomponents/Notification/Notification.tsx
@@ -1,29 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
+import cn from "classnames"
+
 import { Accordion } from "UI"
+
 import s from "./notification.module.scss"
 
 interface NotificationProps {
-  header: React.ReactNode
-  setIsRead: (isRead: boolean) => void
-  children: React.ReactNode
+  read?: boolean
   className?: string
+  header: React.ReactNode
+  children: React.ReactNode
 }
 
 const Notification: React.FC<NotificationProps> = (props) => {
-  const { header, setIsRead, children, className } = props
+  const { header, read, children, className } = props
+  const [isRead, setIsRead] = useState<boolean>(read ?? false)
+
+  const notificationClass = cn(s.notification, className)
+
   return (
-    <div className={className ? s[className] : undefined}>
-      <div className={s.notification} onClick={() => setIsRead(true)}>
-        <Accordion
-          className={s.accordion}
-          header={header}
-          headerClassName={s.header}
-          openTitle="Detail"
-          openTitleClassName={s.openTitle}
-        >
-          {children}
-        </Accordion>
-      </div>
+    <div className={notificationClass} onClick={() => setIsRead(true)}>
+      <Accordion
+        className={s.accordion}
+        header={header}
+        headerClassName={s.header}
+        openTitle="Detail"
+        openTitleClassName={s.openTitle}
+      >
+        {children}
+      </Accordion>
     </div>
   )
 }
