a5fce983b3bfabe888dd54510b5a9cbb8beaa3e3
Sam developer <samandarusmanov1@gmail.com>
2022-05-31 20:39:14 +0500

    added responsive styles

--- Сomponents/SupportSection/SupportSection.tsx
+++ Сomponents/SupportSection/SupportSection.tsx
@@ -1,18 +1,34 @@
-import s from "./SupportSection.module.scss"
 import image from './image.png'
 import Image from "next/image"
 import { Button } from "UI"
+import cn from 'classnames'
+import s from "./SupportSection.module.scss"
+import classNames from 'classnames'
 
 const SupportSection = () => {
     return (
         <section className={s.supportSection}>
-            <div className="img">
-                <Image src={image} alt='keks' width={300} height={350} />
-            </div>
-            <div className="content">
-                <Button>
-                    
-                </Button>
+            <div className={s.wrapper}>
+                <div className={s.img}>
+                    <Image src={image} alt='keks' width={300} height={350} />
+                </div>
+                <div className={s.content}>
+                    <h1 className={s.heading}>Hello</h1>
+                    <h3 className={s.subTitle}>
+                        How we can help you?
+                    </h3>
+                    <p className={s.text}>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo, ducimus soluta et quod sapiente nostrum animi corporis. Minima, praesentium aut.
+                    </p>
+                    <div className={s.buttons}>
+                        <Button className={s.btn}>
+                            I have a question about my order
+                        </Button>
+                        <Button className={cn(s.btn, s.btnItem)} variant="outlined">
+                            I have a different question
+                        </Button>
+                    </div>
+                </div>
             </div>
         </section>
     )
