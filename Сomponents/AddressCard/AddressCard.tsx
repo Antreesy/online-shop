702434461b06b4b2d69442e82ac5c2eb10470399
Sam developer <samandarusmanov1@gmail.com>
2022-06-01 14:55:34 +0500

    updated styles

--- Сomponents/AddressCard/AddressCard.tsx
+++ Сomponents/AddressCard/AddressCard.tsx
@@ -15,8 +15,8 @@ const AddressCard: React.FC<AddressCardProps> = (props) => {
             <div className={s.titleBox}>
                 <div className={s.title}>{title}</div>
                 <div className={s.icons}>
-                    <div><Icon type='edit' color='#8100ef' /> <span> Edit </span></div>
-                    <div><Icon type='trash_can' color='#8100ef' /><span>Delete</span></div>
+                    <div><Icon type='edit' color='#8100ef' /> <span className={s.span}> Edit </span></div>
+                    <div><Icon type='trash_can' color='#8100ef' /><span className={s.span}>Delete</span></div>
                 </div>
             </div>
             <div className={s.content}>
