a06dc3a8bf2f5a666ed81e402577e74f9f3d45d3
Maksim <maks.mexx11101982@gmail.com>
2022-06-02 18:41:09 +0500

    added adaptive

--- UI/ColorPicker/ColorPicker.tsx
+++ UI/ColorPicker/ColorPicker.tsx
@@ -1,8 +1,7 @@
 import React from "react"
-import { pink } from "@mui/material/colors"
 import Checkbox from "@mui/material/Checkbox"
-import { FormControl, FormControlLabel, Radio, RadioGroup } from "@mui/material"
 import { styled } from "@mui/material/styles"
+import Grid from "@mui/material/Grid"
 
 import s from "./colorPcker.module.scss"
 
@@ -14,41 +13,38 @@ const colors = [
   { name: "lightBlue", color: "#487f98" },
   { name: "blue", color: "#251d52" },
 ]
-
-const BpIcon = styled("span")(({color}) => ({
+const BpIcon = styled("span")(({ color }) => ({
   width: 50,
   height: 50,
-  backgroundColor: color
+  backgroundColor: color,
 }))
 
-const BpCheckedIcon = styled(BpIcon)({
+const BpCheckedIcon = styled(BpIcon)(({ color }) => ({
+  display: "block",
+  width: 50,
+  height: 50,
   backgroundImage:
-    "linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))",
-  "&:before": {
-    display: "block",
-    width: 50,
-    height: 50,
-    backgroundImage:
-      "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
-      " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
-      "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
-    content: '""',
-  },
-})
-
+    "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
+    " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
+    "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
+  content: '""',
+  backgroundColor: color,
+}))
 
 const ColorPicker: React.FC = () => {
   return (
-    <div>
-      {colors.map((item) => (
-        <Checkbox
-          key={item.color}
-          disableRipple
-          checkedIcon={<BpCheckedIcon />}
-          icon={<BpIcon color={item.color}/>}
-        />
-      ))}
-    </div>
+    <Grid container>
+      <Grid item md={12} sm={6} xs={6}>
+        {colors.map((item) => (
+          <Checkbox
+            key={item.color}
+            disableRipple
+            checkedIcon={<BpCheckedIcon color={item.color} />}
+            icon={<BpIcon color={item.color} />}
+          />
+        ))}
+      </Grid>
+    </Grid>
   )
 }
 
