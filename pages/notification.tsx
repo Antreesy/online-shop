931b6a3aea81d436c989f0fdcbda85274e2f82cc
Sam developer <samandarusmanov1@gmail.com>
2022-06-03 17:09:16 +0500

    formatted

--- pages/notification.tsx
+++ pages/notification.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next"
 import Head from "next/head"
 
 import { Footer, Header, ProfileSidebar, Notification } from "Сomponents"
-import { Button, Icon, Tabs } from 'UI'
+import { Button, Icon, Tabs } from "UI"
 
 import { footerNav } from "shared/constants/footernav"
 
@@ -20,7 +20,19 @@ const FaqPage: NextPage = () => {
       <main className={s.main}>
         <div className={s.container}>
           <div className={s.notfLabel}>
-            <p><Icon type="bell_filled" className={s.notfIcon} /><div>Notification</div></p> <div ><Button className={s.notfBtn} icon iconLeft="minus" rounded variant="text" /></div>
+            <p>
+              <Icon type="bell_filled" className={s.notfIcon} />
+              <div>Notification</div>
+            </p>{" "}
+            <div>
+              <Button
+                className={s.notfBtn}
+                icon
+                iconLeft="minus"
+                rounded
+                variant="text"
+              />
+            </div>
           </div>
 
           <div className={s.aside}>
@@ -28,80 +40,127 @@ const FaqPage: NextPage = () => {
           </div>
 
           <div className={s.content}>
-
             <Tabs
               className={s.notification_tabs}
               variant="spaces"
               labels={[
                 "Unread Notifications",
                 "Read Notifications",
-                "All Notifications"
+                "All Notifications",
               ]}
               values={[
-
                 <div key={"Unread Notifications"} className={s.notfItem}>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
                 </div>,
 
                 <div key={"Read Notifications"} className={s.notfItem}>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
                 </div>,
 
                 <div key={"All Notifications"} className={s.notfItem}>
-                  <Notification header={'Notification'}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, id sint odit laborum, perferendis eius repudiandae eligendi consequuntur sed numquam quod cumque aspernatur reprehenderit ea, magnam nam omnis delectus accusamus!
+                  <Notification header={"Notification"}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Tempore, id sint odit laborum, perferendis eius repudiandae
+                    eligendi consequuntur sed numquam quod cumque aspernatur
+                    reprehenderit ea, magnam nam omnis delectus accusamus!
                   </Notification>
-
-                </div>
+                </div>,
               ]}
             />
-
           </div>
         </div>
       </main>
