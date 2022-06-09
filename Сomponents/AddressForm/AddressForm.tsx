a130aab08011bec27286cf84147ece3c853450c3
Максим Сухарев <tris-z@mail.ru>
2022-06-09 12:20:11 +0700

    add adaptive

--- Сomponents/AddressForm/AddressForm.tsx
+++ Сomponents/AddressForm/AddressForm.tsx
@@ -1,4 +1,5 @@
 import { Controller, useForm } from "react-hook-form"
+import cn from "classnames"
 
 import { SectionHeader } from "Сomponents"
 import { Button, Icon, Input, Select } from "UI"
@@ -51,139 +52,149 @@ const AddressForm = () => {
         />
 
         <div className={s.row}>
-          <Controller
-            name={"address"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                className={s.input_100}
-                label={"Add Address"}
-                setValue={onChange}
-                value={value}
-                placeholder={"Add Address"}
-              />
-            )}
-          />
+          <div className={cn(s.cell, s.cell_100)}>
+            <Controller
+              name={"address"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  label={"Add Address"}
+                  setValue={onChange}
+                  value={value}
+                  placeholder={"Add Address"}
+                />
+              )}
+            />
+          </div>
         </div>
 
         <div className={s.row}>
-          <Controller
-            name={"name"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                className={s.input_50}
-                label={"Name*"}
-                setValue={onChange}
-                value={value}
-                placeholder={"Name*"}
-              />
-            )}
-          />
-          <Controller
-            name={"surname"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                className={s.input_50}
-                label={"Surname*"}
-                setValue={onChange}
-                value={value}
-                placeholder={"Surname*"}
-              />
-            )}
-          />
+          <div className={cn(s.cell, s.cell_50)}>
+            <Controller
+              name={"name"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  label={"Name*"}
+                  setValue={onChange}
+                  value={value}
+                  placeholder={"Name*"}
+                />
+              )}
+            />
+          </div>
+          <div className={cn(s.cell, s.cell_50)}>
+            <Controller
+              name={"surname"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  label={"Surname*"}
+                  setValue={onChange}
+                  value={value}
+                  placeholder={"Surname*"}
+                />
+              )}
+            />
+          </div>
         </div>
 
         <div className={s.row}>
-          <Controller
-            name={"country"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Select
-                className={s.input_20}
-                label="Country Code"
-                placeholder="Country Code*"
-                initValue={value.value}
-                values={countriesForSelect()}
-                onChange={onChange}
-              />
-            )}
-          />
-          <Controller
-            name={"phone"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                className={s.input_30}
-                label={"Phone Number"}
-                setValue={onChange}
-                value={value}
-                placeholder={"Phone Number"}
-              />
-            )}
-          />
+          <div className={cn(s.cell, s.cell_50)}>
+            <Controller
+              name={"country"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Select
+                  className={s.input_20}
+                  label="Country Code"
+                  placeholder="Country Code*"
+                  initValue={value.value}
+                  values={countriesForSelect()}
+                  onChange={onChange}
+                />
+              )}
+            />
 
-          <Controller
-            name={"city"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Select
-                className={s.input_50}
-                label="City"
-                placeholder="City*"
-                initValue={value.value}
-                values={[]}
-                onChange={onChange}
-              />
-            )}
-          />
+            <Controller
+              name={"phone"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  className={s.input_30}
+                  label={"Phone Number"}
+                  setValue={onChange}
+                  value={value}
+                  placeholder={"Phone Number"}
+                />
+              )}
+            />
+          </div>
+
+          <div className={cn(s.cell, s.cell_50)}>
+            <Controller
+              name={"city"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Select
+                  label="City"
+                  placeholder="City*"
+                  initValue={value.value}
+                  values={[]}
+                  onChange={onChange}
+                />
+              )}
+            />
+          </div>
         </div>
 
         <div className={s.row}>
-          <Controller
-            name={"something"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                className={s.input_50}
-                label={"Something"}
-                setValue={onChange}
-                value={value}
-                placeholder={"Something"}
-              />
-            )}
-          />
-          <Controller
-            name={"street"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                className={s.input_50}
-                label={"Street*"}
-                setValue={onChange}
-                value={value}
-                placeholder={"Street*"}
-              />
-            )}
-          />
+          <div className={cn(s.cell, s.cell_50)}>
+            <Controller
+              name={"something"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  label={"Something"}
+                  setValue={onChange}
+                  value={value}
+                  placeholder={"Something"}
+                />
+              )}
+            />
+          </div>
+          <div className={cn(s.cell, s.cell_50)}>
+            <Controller
+              name={"street"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  label={"Street*"}
+                  setValue={onChange}
+                  value={value}
+                  placeholder={"Street*"}
+                />
+              )}
+            />
+          </div>
         </div>
 
         <SectionHeader className={s.header} title="Home" />
 
         <div className={s.row}>
-          <Controller
-            name={"description"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                className={s.input_100}
-                setValue={onChange}
-                value={value}
-                placeholder={"Address"}
-              />
-            )}
-          />
+          <div className={cn(s.cell, s.cell_100)}>
+            <Controller
+              name={"description"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  setValue={onChange}
+                  value={value}
+                  placeholder={"Address"}
+                />
+              )}
+            />
+          </div>
         </div>
 
         <Button onClick={onSubmit} className={s.save_btn}>
