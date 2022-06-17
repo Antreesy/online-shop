3c19b27a41613ff4a599763788dd40ee583ab3ac
Максим Сухарев <tris-z@mail.ru>
2022-06-17 11:49:47 +0200

    minor fixes

--- pages/brand-information.tsx
+++ pages/brand-information.tsx
@@ -1,33 +1,34 @@
 import { NextPage } from "next"
 import { Controller, useForm } from "react-hook-form"
-import Head from "next/head"
+import cn from "classnames"
 
+import Head from "next/head"
+import SimpleBar from "simplebar-react"
 import { Button, FileUpload, Icon, Input } from "UI"
 
-import cn from "classnames"
 import s from "styles/pages/brand-information.module.scss"
 
 interface BrandsType {
-  brandname: string
+  brandName: string
   name: string
   sellerId: string
   number: string
   taxAdmin: string
   kepAddress: string
-  taxnumber: string
+  taxNumber: string
 }
 
 const brandInformation: NextPage = () => {
   const { handleSubmit, control } = useForm<BrandsType>({
     criteriaMode: "all",
     defaultValues: {
-      brandname: "",
+      brandName: "",
       name: "",
       sellerId: "",
       number: "",
       taxAdmin: "",
       kepAddress: "",
-      taxnumber: "",
+      taxNumber: "",
     },
   })
   const onSubmit = handleSubmit((data) => {
@@ -40,8 +41,8 @@ const brandInformation: NextPage = () => {
         <title>ILONSI SHOP | Brand information</title>
       </Head>
 
-      <div className={s.container}>
-        <div className={s.nav}>
+      <nav className={s.nav}>
+        <SimpleBar className={s.scrollbar}>
           <ul className={s.navigation}>
             <li>Profile</li>
             <li>Brand Information</li>
@@ -51,75 +52,77 @@ const brandInformation: NextPage = () => {
             <li>That Need Revision</li>
             <li>Not On Sale</li>
           </ul>
-        </div>
-        <Button iconLeft="arrow_left" className={s.backBtn} variant="text">
-          Back
-        </Button>
-        <h1>Brand Informations</h1>
+        </SimpleBar>
+      </nav>
 
-        <div className={s.brandInfo}>
-          <div className={s.brandItem}>
-            <div className={s.iconWrapper}>
-              <Icon type="brand_name" />
-            </div>
-            <div className={s.textBlock}>
-              <p className={s.brandName}>Brand Name:</p>
-              <p className={s.description}>Saint Laurent</p>
-            </div>
-          </div>
-          <div className={s.brandDisplay}>
-            <div className={s.iconWrapper}>
-              <Icon type="brand_display_name" />
-            </div>
-            <div className={s.textBlock}>
-              <p className={s.brandName}>Brand Display Name:</p>
-              <p className={s.description}> Saint Lauren</p>
-            </div>
-          </div>
-        </div>
+      <Button iconLeft="arrow_left" className={s.backBtn} variant="text">
+        Back
+      </Button>
+      <h1 className={s.heading}>Brand Informations</h1>
 
-        <div className={s.brandUploads}>
-          <div className={s.uploadLogo}>
-            <p>Brand Logo:</p>
-            <div className={s.uploader}>
-              <FileUpload
-                title="Select or Drag Image"
-                setValue={() => console.log("")}
-                className={s.fileUploader}
-              />
-            </div>
-            <Button variant="text" className={s.brandBlackBtn}>
-              Load
-            </Button>
+      <div className={s.brandInfo}>
+        <div className={s.brandItem}>
+          <div className={s.iconWrapper}>
+            <Icon type="brand_name" />
           </div>
-          <div className={s.uploadBanner}>
-            <p>Brand Banner:</p>
-            <div className={s.uploader}>
-              <FileUpload
-                title="Select or Drag Image"
-                setValue={() => console.log("")}
-                className={s.fileUploader}
-              />
-            </div>
-            <Button variant="text" className={s.brandBlackBtn}>
-              Load
-            </Button>
+          <div className={s.textBlock}>
+            <p className={s.brandName}>Brand Name:</p>
+            <p className={s.description}>Saint Laurent</p>
           </div>
         </div>
-        <h1>Company Informations</h1>
+        <div className={s.brandDisplay}>
+          <div className={s.iconWrapper}>
+            <Icon type="brand_display_name" />
+          </div>
+          <div className={s.textBlock}>
+            <p className={s.brandName}>Brand Display Name:</p>
+            <p className={s.description}> Saint Lauren</p>
+          </div>
+        </div>
       </div>
+
+      <div className={s.brandUploads}>
+        <div className={s.uploadLogo}>
+          <p>Brand Logo:</p>
+          <div className={s.uploader}>
+            <FileUpload
+              title="Select or Drag Image"
+              setValue={() => console.log("")}
+              className={s.fileUploader}
+            />
+          </div>
+          <Button variant="text" className={s.brandBlackBtn}>
+            Load
+          </Button>
+        </div>
+        <div className={s.uploadBanner}>
+          <p>Brand Banner:</p>
+          <div className={s.uploader}>
+            <FileUpload
+              title="Select or Drag Image"
+              setValue={() => console.log("")}
+              className={s.fileUploader}
+            />
+          </div>
+          <Button variant="text" className={s.brandBlackBtn}>
+            Load
+          </Button>
+        </div>
+      </div>
+      <h1>Company Informations</h1>
+
       <div className={s.formSection}>
         <form className={s.container}>
           <div className={s.firstLineInputs}>
             <Controller
-              name={"brandname"}
+              name={"brandName"}
               control={control}
               render={({ field: { onChange, value } }) => (
                 <Input
                   label={"Brand Name"}
                   setValue={onChange}
                   value={value}
-                  placeholder={"Name Surname"}
+                  placeholder={"Brand Name"}
                   className={s.input}
                 />
               )}
@@ -129,10 +132,10 @@ const brandInformation: NextPage = () => {
               control={control}
               render={({ field: { onChange, value } }) => (
                 <Input
-                  label={"Cari Ünvan  "}
+                  label={"Cari Ünvan"}
                   setValue={onChange}
                   value={value}
-                  placeholder={"Name Surname"}
+                  placeholder={"Cari Ünvan"}
                   className={s.input}
                 />
               )}
@@ -145,7 +148,7 @@ const brandInformation: NextPage = () => {
                   label={"Seller ID"}
                   setValue={onChange}
                   value={value}
-                  placeholder={"Name Surname"}
+                  placeholder={"Seller ID"}
                   className={s.input}
                 />
               )}
@@ -160,7 +163,7 @@ const brandInformation: NextPage = () => {
                   label={"Mersis No"}
                   setValue={onChange}
                   value={value}
-                  placeholder={"Name Surname"}
+                  placeholder={"Mersis No"}
                   className={s.input}
                 />
               )}
@@ -173,7 +176,7 @@ const brandInformation: NextPage = () => {
                   label={"Tax Administration"}
                   setValue={onChange}
                   value={value}
-                  placeholder={"Name Surname"}
+                  placeholder={"Tax Administration"}
                   className={s.input}
                 />
               )}
@@ -186,7 +189,7 @@ const brandInformation: NextPage = () => {
                   label={"KEP Address"}
                   setValue={onChange}
                   value={value}
-                  placeholder={"Name Surname"}
+                  placeholder={"KEP Address"}
                   className={s.input}
                 />
               )}
@@ -200,7 +203,7 @@ const brandInformation: NextPage = () => {
                 label={"Tax number"}
                 setValue={onChange}
                 value={value}
-                placeholder={"Name Surname"}
+                placeholder={"Tax number"}
                 className={s.inputTextArea}
               />
             )}
