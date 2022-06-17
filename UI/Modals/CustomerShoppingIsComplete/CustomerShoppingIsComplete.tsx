e329f39b50ba8136ecd842e90a37ff0d44d66938
linkoln <79755659+linkoln-1@users.noreply.github.com>
2022-06-17 12:02:33 +0300

    corrected adaptive modals

--- UI/Modals/CustomerShoppingIsComplete/CustomerShoppingIsComplete.tsx
+++ UI/Modals/CustomerShoppingIsComplete/CustomerShoppingIsComplete.tsx
@@ -1,29 +1,29 @@
-import React from "react"
-import s from "./CustomerShoppingIsComplete.module.scss"
-import { Button } from "../../Button"
+    import React from "react"
+    import s from "./CustomerShoppingIsComplete.module.scss"
+    import { Button } from "../../Button"
 
-interface CustomerShoppingIsComplete {
-  labels: {
-    tittle: string
-    content: string
-  }
-}
+    interface CustomerShoppingIsComplete {
+      labels: {
+        tittle: string
+        content: string
+      }
+    }
 
-export const CustomerShoppingIsComplete: React.FC<
-  CustomerShoppingIsComplete
-> = ({ labels }) => {
-  return (
-    <>
-      <div className={s.tittle}>
-        <p>{labels.tittle}</p>
-      </div>
-      <div className={s.content}>
-        <p>{labels.content}</p>
-      </div>
-      <div className={s.button}>
-        <Button className={s.continue}>Continue Shopping</Button>
-        <Button className={s.close}>Close</Button>
-      </div>
-    </>
-  )
-}
+    export const CustomerShoppingIsComplete: React.FC<
+      CustomerShoppingIsComplete
+    > = ({ labels }) => {
+      return (
+        <>
+          <div className={s.tittle}>
+            <p>{labels.tittle}</p>
+          </div>
+          <div className={s.content}>
+            <p>{labels.content}</p>
+          </div>
+          <div className={s.button}>
+            <Button className={s.continue}>Continue Shopping</Button>
+            <Button className={s.close}>Close</Button>
+          </div>
+        </>
+      )
+    }
