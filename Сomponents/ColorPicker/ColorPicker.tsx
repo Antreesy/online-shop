b5e5c655e76c29aa35d46b3e5af1928a22854f0e
alivan-js <ivanowagner@hotmail.com>
2022-06-19 13:30:09 +0300

    feat(ProductPage): deleted comments in scss and added Color Interface for ColorPicker

--- Сomponents/ColorPicker/ColorPicker.tsx
+++ Сomponents/ColorPicker/ColorPicker.tsx
@@ -13,8 +13,12 @@ const BpCheckedIcon = styled(BpIcon)(() => ({
     "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
 }))
 
+interface Color {
+  name: string; color: string
+}
+
 interface ColorPickerProps {
-  colors: { name: string; color: string }[]
+  colors: Color[]
   className: string
 }
 
