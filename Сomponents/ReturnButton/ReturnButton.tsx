a37481362371dd2ff0255ab3174a7a08b0a0391d
Максим Сухарев <tris-z@mail.ru>
2022-06-22 16:04:42 +0200

    minor fixes

--- Сomponents/ReturnButton/ReturnButton.tsx
+++ Сomponents/ReturnButton/ReturnButton.tsx
@@ -1,16 +1,21 @@
 import { Button } from "UI"
+import { useRouter } from "next/router"
+import { useTranslation } from "next-i18next"
 
 import s from "./returnButton.module.scss"
-import { useTranslation } from "next-i18next"
-import {useRouter} from "next/router";
 
 const ReturnButton: React.FC = () => {
   const { t } = useTranslation("product")
-    const router = useRouter()
+  const router = useRouter()
   return (
     <div className={s.button_wrapper}>
-      <Button icon iconLeft="arrow_left" variant="text" className={s.button} onClick={() => router.back()} />
-      <span>{t("productsTopButton")}</span>
+      <Button
+        icon
+        iconLeft="arrow_left"
+        className={s.button}
+        onClick={() => router.back()}
+      />
+      {t("productsTopButton")}
     </div>
   )
 }
