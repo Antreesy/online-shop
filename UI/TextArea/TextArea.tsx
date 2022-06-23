645c7870e0e07008f2daef29699beca96a22aa80
Maksim <maks.mexx11101982@gmail.com>
2022-06-23 22:27:37 +0500

    change input on TextArea and added props className

--- UI/TextArea/TextArea.tsx
+++ UI/TextArea/TextArea.tsx
@@ -1,16 +1,24 @@
 import TextareaAutosize from "@mui/material/TextareaAutosize"
+import cn from "classnames"
 import s from "./textArea.module.scss"
 
 interface TextAriaProps {
-  size?: object
+  style?: object
   placeholder?: string
+  className?: object
   value: string
   setValue: (newValue: string) => void
   onChange?: () => void
 }
 
 const CustomTextArea: React.FC<TextAriaProps> = (props) => {
-  const { value, size, placeholder = "", onChange, setValue } = props
+  const {
+    value,
+    placeholder = "",
+    className = "",
+    onChange,
+    setValue,
+  } = props
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(event.target.value)
@@ -21,9 +29,8 @@ const CustomTextArea: React.FC<TextAriaProps> = (props) => {
     <TextareaAutosize
       onChange={handleChange}
       value={value}
-      className={s.aria}
+      className={cn(s.aria, className)}
       placeholder={placeholder}
-      style={size}
     />
   )
 }
