eada125e18c79af1680cc6678af0f7dfb366b7cb
linkoln <79755659+linkoln-1@users.noreply.github.com>
2022-06-23 17:09:13 +0300

    corrected button back for 2 components

--- Сomponents/ReturnButton/ReturnButton.tsx
+++ Сomponents/ReturnButton/ReturnButton.tsx
@@ -8,12 +8,11 @@ const ReturnButton: React.FC = () => {
   const { t } = useTranslation("product")
   const router = useRouter()
   return (
-    <div className={s.button_wrapper}>
+    <div className={s.button_wrapper}  onClick={() => router.back()}>
       <Button
         icon
         iconLeft="arrow_left"
         className={s.button}
-        onClick={() => router.back()}
       />
       {t("productsTopButton")}
     </div>
