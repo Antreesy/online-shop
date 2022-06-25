7b6becc81bbee3a87d1c49e92da99df6ea8e0b63
Максим Сухарев <tris-z@mail.ru>
2022-06-25 12:09:53 +0200

    minor fixes

--- Сomponents/CartItemList/CartItemList.tsx
+++ Сomponents/CartItemList/CartItemList.tsx
@@ -1,4 +1,5 @@
-import { Button } from "UI"
+import { useTranslation } from "next-i18next"
+
 import { CartItem, CartItemProps } from "./CartItem/CartItem"
 
 import s from "./cartItemList.module.scss"
@@ -8,30 +9,23 @@ interface CartListProps {
 }
 
 export const CartItemList: React.FC<CartListProps> = ({ list }) => {
+  const { t } = useTranslation("order")
+
   return (
     <>
       {list.length ? (
-        list.map((item, index) => {
-          return <CartItem {...item} key={index} />
-        })
-      ) : (
-        <div className={s.empty_wrapper}>
-          <Button
-            icon
-            iconLeft="basket"
-            className={s.empty_button}
-            variant="text"
-          />
-          <p className={s.title}>There are no items in your cart</p>
-          <p className={s.subtitle}>Add items to your cart to buy</p>
-        </div>
-      )}
-
-{/* {
-  list.map((item, index) => {
-          return <CartItem {...item} key={index} />
-        })
-} */}
+        <>
+          <div className={s.cart_header}>
+            <p className={s.names}>{t("nameOfTheProduct")}</p>
+            <p className={s.price}>{t("price")}</p>
+            <p className={s.piece}>{t("piece")}</p>
+            <p className={s.total}>{t("total")}</p>
+          </div>
+          {list.map((item, index) => (
+            <CartItem {...item} key={index} />
+          ))}
+        </>
+      ) : null}
     </>
   )
 }
